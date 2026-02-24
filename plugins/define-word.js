b0e2d23f0dbbb93fc52516e6a911fc9495ebcfb0
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2026-02-25 01:52:15 +0200

    Update define-word.js

--- plugins/define-word.js
+++ plugins/define-word.js
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, q, reply }) => {
 ✍️ *Example*: ${example}  
 📝 *Synonyms*: ${synonyms}  
 
-🔗 *ᗩᗪᗴᗴᒪ-᙭ᗰᗪ*`;
+🔗 *𝚳𝐒𝚵𝐋𝚫-𝐂𝚮𝐔𝚰-𝚾𝚳𝐃*`;
 
         if (audio) {
             await conn.sendMessage(from, { audio: { url: audio }, mimetype: 'audio/mpeg' }, { quoted: mek });
